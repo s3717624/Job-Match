195026d972406a936528fbe8f5146054d9e1a8b1
Kevin Nguyen <s3719491@student.rmit.edu.au>
2020-10-06 12:43:55 +1100

    removed page numbers

--- jobhub/listing.php
+++ jobhub/listing.php
@@ -651,17 +651,6 @@ if(isset($_GET['search_query']))
                             <div class="container">
                                 <div class="row">
                                     <div class="col-xl-12">
-                                        <div class="single-wrap d-flex justify-content-center">
-                                            <nav aria-label="Page navigation example">
-                                                <ul class="pagination justify-content-start " id="myDIV">
-                                                    <li class="page-item"><a class="page-link" href="#"><span class="ti-angle-left"></span></a></li>
-                                                    <li class="page-item active"><a class="page-link" href="#">01</a></li>
-                                                    <li class="page-item"><a class="page-link" href="#">02</a></li>
-                                                    <li class="page-item"><a class="page-link" href="#">03</a></li>
-                                                    <li class="page-item"><a class="page-link" href="#"><span class="ti-angle-right"></span></a></li>
-                                                </ul>
-                                            </nav>
-                                        </div>
                                     </div>
                                 </div>
                             </div>
