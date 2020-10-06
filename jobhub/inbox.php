c685fb0cdc1e0e3cd057d9b0074ad568e3447686
Linus Cheung <s3717624@student.rmit.edu.au>
2020-10-06 12:45:15 +1100

    add header

--- jobhub/inbox.php
+++ jobhub/inbox.php
@@ -51,70 +51,7 @@ $_SESSION['currentpage'] = "inbox.php";
 
 <!-- Preloader Start -->
 <!-- Header Start -->
-<div class="header-area header-transparent">
-    <div class="main-header ">
-        <div class="header-bottom  header-sticky">
-            <div class="container-fluid">
-                <div class="row align-items-center">
-                    <!-- Logo -->
-                    <div class="col-xl-2 col-lg-2">
-                        <div class="logo">
-                            <a href="index.php"><img src="assets/img/logo/logo.png" alt=""></a>
-                        </div>
-                    </div>
-                    <div class="col-xl-10 col-lg-10">
-                        <div class="menu-wrapper  d-flex align-items-center justify-content-end">
-                            <!-- Main-menu -->
-                            <div class="main-menu d-none d-lg-block">
-                                <nav>
-                                    <ul id="navigation">
-                                        <li><a href="index.php">Home</a></li>
-                                        <li><a href="listing.php">Listings</a></li>       
-                                        <?php
-                                        $add_job_text = "";
-                                        $modified_link2 = "";
-
-                                        if (isset($_SESSION["user_id"]))
-                                        {
-                                            $add_job_text = "Add job";
-                                            $modified_link2 = "<a href='add_job.php'>";
-                                            echo "<li>".$modified_link2.$add_job_text."</a></li>";
-                                        }
-                                        ?>
-                                        <li><a href="categori.html">Categories</a></li>
-                                        <li><a href="#">Pages</a>
-                                            <ul class="submenu">
-                                                <li><a href="about.html">about</a></li>
-                                                <li><a href="blog.html">Blog</a></li>
-                                                <li><a href="blog_details.html">Blog Details</a></li>
-                                                <li><a href="elements.html">Element</a></li>
-                                            </ul>
-                                        </li>
-                                       
-                                        <li><a href="php/logging_out.php">Log out</a></li>
-                                        
-
-                                    </ul>
-                                </nav>
-                            </div>
-                            <!-- Header-btn -->
-                            <div class="header-right-btn d-none d-lg-block ml-65">
-                                <a href="add_job.php" class="border-btn">Post a Job</a>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Mobile Menu -->
-                    <div class="col-12">
-                        <div class="mobile_menu d-block d-lg-none"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- Header End -->
-</header>
-<!-- header end -->
+<?php include_once("header.php") ?>
 <!-- Hero Area Start-->
 <div class="slider-area">
     <div class="single-slider slider-height d-flex align-items-center">
--- jobhub/index.php
+++ jobhub/index.php
@@ -47,71 +47,7 @@ $Account = new Account();
 <!--        </div>-->
 <!--    </div>-->
     <!-- Preloader Start -->
-    <header>
-        <!-- Header Start -->
-        <div class="header-area header-transparent">
-            <div class="main-header ">
-                <div class="header-bottom  header-sticky">
-                    <div class="container-fluid">
-                        <div class="row align-items-center">
-                            <!-- Logo -->
-                            <div class="col-xl-2 col-lg-2">
-                                <div class="logo">
-                                    <a href="index.php"><img src="assets/img/logo/logo.png" alt=""></a>
-                                </div>
-                            </div>
-                            <div class="col-xl-10 col-lg-10">
-                                <div class="menu-wrapper  d-flex align-items-center justify-content-end">
-                                    <!-- Main-menu -->
-                                    <div class="main-menu d-none d-lg-block">
-                                        <nav>
-                                            <ul id="navigation">                                                                                          
-                                                <li><a href="index.php">Home</a></li>
-                                                <li><a href="listing.php">Listings</a></li>
-                                                <?php
-                                                $add_job_text = "";
-                                                $modified_link2 = "";
-
-                                                if (isset($_SESSION["user_id"]) && ($Account->typeCheck($_SESSION['user_id'])) == 'employers')
-                                                {
-                                                    $add_job_text = "Add job";
-                                                    $modified_link2 = "<a href='add_job.php'>";
-                                                    echo "<li>".$modified_link2.$add_job_text."</a></li>";
-                                                    echo "<li><a href='joblist.php'>Your Listings</a></li>";
-                                                }
-                                                ?>
-                                                <li><?php
-                                                    $login_text = "Log in";
-                                                    $modified_link = "<a href='login.php'>";
-
-                                                    if (isset($_SESSION["user_id"]))
-                                                    {
-                                                        $login_text = "Profile";
-                                                        $modified_link = "<a href='profile.php'>";
-                                                    }
-                                                    ?><?php echo $modified_link.$login_text; ?></a></li>
-                                                    <li><a href="inbox.php">Inbox</a></li>
-                                            </ul>
-                                        </nav>
-                                    </div>
-                                    <!-- Header-btn -->
-                                    <div class="header-right-btn d-none d-lg-block ml-65">
-                                        <a href="add_job.php" class="border-btn">Post a Job</a>
-                                    </div>
-                                </div>
-                            </div> 
-                            <!-- Mobile Menu -->
-                            <div class="col-12">
-                                <div class="mobile_menu d-block d-lg-none"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Header End -->
-    </header>
-    <!-- header end -->
+    <?php include_once("header.php") ?>
     <main>
         <!-- Hero Area Start-->
         <div class="slider-area">
